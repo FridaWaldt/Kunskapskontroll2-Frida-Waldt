b80af1117cd4bc19510ea5bcc61cbb1ecb2279d8
Frida Waldt <fridawaldt@gmail.com>
2020-11-19 14:10:06 +0100

    Backgrundsändring
    Fixade så att bakgrunden ändras pga temperaturen

--- app.js
+++ app.js
@@ -2,11 +2,11 @@
 1. All info ska presenteras i metric units - CHECK
 2. Ett formulär med en text-input och en submit-knapp där användaren anger en stad. - CHECK
 3. Det nuvarande vädret för den inmatade staden hämtas och  presenteras. Du behöver ha med följande: 
-    a. Description
-    b. Väderikon, se https://openweathermap.org/weather-conditions 
-    c. Temperatur
-    d. Vindhastighet
-    e. Luftfuktighet
+    a. Description -CHECK
+    b. Väderikon, se https://openweathermap.org/weather-conditions - CHECK
+    c. Temperatur - CHECK
+    d. Vindhastighet - CHECK
+    e. Luftfuktighet - CHECK
 4. Temperaturdatan ska användas för att ändra färg på något i appen. Kan vara hela bakgrunden eller en liten ikon, du väljer själv.
 5. Utifall den inmatade staden inte kan hittas ska det visas ett tydligt och informerande meddelande.
 6. Kommentera all kod med beskrivning av vad den gör
@@ -53,21 +53,21 @@ form.addEventListener('submit',
                 const humid = data.main.humidity;
                 console.log(data.weather[0].icon);
 
-
                 presentData(searchedCity, description, icon, temp, wind, humid); 
+                changeBackground(temp);
             }
         )
         .catch(function (error){
-            removeData()
-            console.log('Jag är i catch')
+            removeData();
+            console.log('Jag är i catch', error)
             const catchError = document.querySelector('#catch-error');
             catchError.innerText = "Incorrect city, try again";
-            
         })
     }
 )
 
-/* För att få fram land: data.sys.country */
+
+//En funktion som presenterar data från input
 
 function presentData(n, d, i, t, w, h){
     let cityName = document.querySelector('#city-name')
@@ -75,32 +75,35 @@ function presentData(n, d, i, t, w, h){
     let weatherIcon = document.querySelector('#weather-icon');
     let temperature = document.querySelector('#temperature');
     let windSpeed = document.querySelector('#wind-speed');
+    let windSpeedTitle = document.querySelector('#wind-speed-title');
     let humidity = document.querySelector('#humidity');
+    let humidityTitle = document.querySelector('#humidity-speed-title');
     let error = document.querySelector('#catch-error');
 
     cityName.innerText = n;
     cityDescription.innerText = d;
+    cityDescription.style.textTransform = 'capitalize';
     weatherIcon.src = `http://openweathermap.org/img/wn/${i}@2x.png`;
-    temperature.innerText = t;
-    windSpeed.innerText = w;
-    humidity.innerText = h;
+    temperature.innerHTML = `${t}&#8451;`;
+    windSpeed.innerText = `${w} m/s`;
+    windSpeedTitle.innerText = 'Wind Speed';
+    humidity.innerText = `${h}%`;
+    humidityTitle.innerText = 'Humidity';
     error.innerText = '';
-
-
-
 }
-
 
 
 // en funktion som tar bort data från .catch
 
 function removeData(){
-    let cityName = document.querySelector('#city-name')
+    let cityName = document.querySelector('#city-name');
     let cityDescription = document.querySelector('#description');
     let weatherIcon = document.querySelector('#weather-icon');
     let temperature = document.querySelector('#temperature');
     let windSpeed = document.querySelector('#wind-speed');
+    let windSpeedTitle = document.querySelector('#wind-speed-title');
     let humidity = document.querySelector('#humidity');
+    let humidityTitle = document.querySelector('#humidity-speed-title');
     let error = document.querySelector('#catch-error');
 
     cityName.innerText = '';
@@ -108,6 +111,32 @@ function removeData(){
     weatherIcon.src = '';
     temperature.innerText = '';
     windSpeed.innerText = '';
+    windSpeedTitle.innerText = '';
     humidity.innerText = '';
+    humidityTitle.innerText = '';
     error.innerText = '';
 }
+
+
+//en funktion som ändrar bakgrund
+function changeBackground(temp){
+    let background = document.querySelector('.background');
+
+    if (temp <= 5){
+        background.style.backgroundImage = "linear-gradient(rgba(255,255,255,0.5), rgba(255,255,255,0.5)), url('img/winter.jpg')";
+    }
+    else if(temp > 5 && temp < 10){
+        background.style.backgroundImage = "linear-gradient(rgba(255,255,255,0.5), rgba(255,255,255,0.5)), url('img/fall.jpg')";
+    }
+    else if(temp > 10 && temp < 20){
+        background.style.backgroundImage = "linear-gradient(rgba(255,255,255,0.5), rgba(255,255,255,0.5)), url('img/spring.jpg')";
+    }
+    else{
+        background.style.backgroundImage = "linear-gradient(rgba(255,255,255,0.5), rgba(255,255,255,0.5)), url('img/summer.jpg')";
+    }
+
+}
+
+
+
+/* För att få fram land: data.sys.country */
